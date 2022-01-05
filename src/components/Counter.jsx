41b5ed2f4e25314f4c8de23c6016ecd0242c1d69
Isaque Igor <isaqueigorsilva@gmail.com>
2022-01-04 21:43:19 -0300

    feat: fast-refresh webpack

---
+++ src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function Counter() {
+    const [counter, setCounter] = useState(0)
+
+    const increment = () => {
+        setCounter(counter +1)
+    }
+
+    return (
+        <div>
+            <h2>{counter}</h2>
+            <button type='button' onClick={increment}>Increment +1</button>
+        </div>
+    )
+}
